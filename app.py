8cac787980c0b421a66101b40e7a7854975ce084
kennedy Ndungu <kenyxndush@gmail.com>
2021-06-02 17:18:07 +0300

    some changes

--- app.py
+++ app.py
@@ -1,259 +1,91 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
-from passlib.hash import sha256_crypt
-from functools import wraps
-from flask_uploads import UploadSet, configure_uploads, IMAGES
-import timeit
-import datetime
-from flask_mail import Mail, Message
-import os
-from wtforms.fields.html5 import EmailField
-import secrets
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.secret_key = 'secret'
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/images'
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-# Config MySQL
-mysql = MySQL()
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'fitness'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
-# Initialize the app for use with this MySQL class
-mysql.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, *kwargs)
-        else:
-            return redirect(url_for('login'))
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-    return wrap
-
-
-def not_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return redirect(url_for('index'))
-        else:
-            return f(*args, *kwargs)
-
-    return wrap
-
-
-def is_admin_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'admin_logged_in' in session:
-            return f(*args, *kwargs)
-        else:
-            return redirect(url_for('admin_login'))
-
-    return wrap
-
-
-def not_admin_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'admin_logged_in' in session:
-            return redirect(url_for('admin'))
-        else:
-            return f(*args, *kwargs)
-
-    return wrap
-
-
-def wrappers(func, *args, **kwargs):
-    def wrapped():
-        return func(*args, **kwargs)
-
-    return wrapped
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-class LoginForm(Form):  # Create Login Form
-    username = StringField('', [validators.length(min=1)],
-                           render_kw={'autofocus': True, 'placeholder': 'Username'})
-    password = PasswordField('', [validators.length(min=3)],
-                             render_kw={'placeholder': 'Password'})
 
-
-# User Login
-@app.route('/login', methods=['GET', 'POST'])
-@not_logged_in
-def login():
-    form = LoginForm(request.form)
-    if request.method == 'POST' and form.validate():
-        # GEt user form
-        username = form.username.data
-        # password_candidate = request.form['password']
-        password_candidate = form.password.data
-
-        # Create cursor
-        cur = mysql.connection.cursor()
-
-        # Get user by username
-        result = cur.execute("SELECT * FROM users WHERE username=%s", [username])
-
-        if result > 0:
-            # Get stored value
-            data = cur.fetchone()
-            password = data['password']
-            uid = data['id']
-            name = data['name']
-
-            # Compare password
-            if sha256_crypt.verify(password_candidate, password):
-                # passed
-                session['logged_in'] = True
-                session['uid'] = uid
-                session['s_name'] = name
-                x = '1'
-                cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
-
-                return redirect(url_for('index'))
-
-            else:
-                flash('Incorrect password', 'danger')
-                return render_template('login.html', form=form)
-
-        else:
-            flash('Username not found', 'danger')
-            # Close connection
-            cur.close()
-            return render_template('login.html', form=form)
-    return render_template('login.html', form=form)
-
-
-@app.route('/logout')
-def logout():
-    if 'uid' in session:
-        # Create cursor
-        cur = mysql.connection.cursor()
-        uid = session['uid']
-        x = '0'
-        cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
-        session.clear()
-        flash('You are logged out', 'success')
-        return redirect(url_for('index'))
-    return redirect(url_for('login'))
-
-
-class RegisterForm(Form):
-    name = StringField('', [validators.length(min=3, max=50)],
-                       render_kw={'autofocus': True, 'placeholder': 'Full Name'})
-    username = StringField('', [validators.length(min=3, max=25)], render_kw={'placeholder': 'Username'})
-    email = EmailField('', [validators.DataRequired(), validators.Email(), validators.length(min=4, max=25)],
-                       render_kw={'placeholder': 'Email'})
-    password = PasswordField('', [validators.length(min=3)],
-                             render_kw={'placeholder': 'Password'})
-    mobile = StringField('', [validators.length(min=10, max=15)], render_kw={'placeholder': 'Mobile'})
-
-
-@app.route('/register', methods=['GET', 'POST'])
-@not_logged_in
-def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
-        mobile = form.mobile.data
-
-        # Create Cursor
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email, username, password, mobile) VALUES(%s, %s, %s, %s, %s)",
-                    (name, email, username, password, mobile))
-
-        # Commit cursor
-        mysql.connection.commit()
-
-        # Close Connection
-        cur.close()
-
-        flash('You are now registered and can login', 'success')
-
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
-
-@app.route('/profile')
-@is_logged_in
-def profile():
-    if 'user' in request.args:
-        q = request.args['user']
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE id=%s", (q,))
-        result = cur.fetchone()
-    else:
-        flash('Unauthorised', 'danger')
-        return redirect(url_for('login'))
- 
-class UpdateRegisterForm(Form):
-    name = StringField('Full Name', [validators.length(min=3, max=50)],
-                       render_kw={'autofocus': True, 'placeholder': 'Full Name'})
-    email = EmailField('Email', [validators.DataRequired(), validators.Email(), validators.length(min=4, max=25)],
-                       render_kw={'placeholder': 'Email'})
-    password = PasswordField('Password', [validators.length(min=3)],
-                             render_kw={'placeholder': 'Password'})
-    mobile = StringField('Mobile', [validators.length(min=10, max=15)], render_kw={'placeholder': 'Mobile'})
- 
 @app.route('/progress')
 def progress():
-    return render_template("progress.html")
+    return render_template('progress.html')
 
-@app.route('/reviews')
-def reviews():
-    return render_template("reviews.html")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 
-@app.route('/settings', methods=['POST', 'GET'])
-@is_logged_in
-def settings():
-    form = UpdateRegisterForm(request.form)
-    if 'user' in request.args:
-        q = request.args['user']
-        curso = mysql.connection.cursor()
-        curso.execute("SELECT * FROM users WHERE id=%s", (q,))
-        result = curso.fetchone()
-        if result:
-            if result['id'] == session['uid']:
-                if request.method == 'POST' and form.validate():
-                    name = form.name.data
-                    email = form.email.data
-                    password = sha256_crypt.encrypt(str(form.password.data))
-                    mobile = form.mobile.data
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('index'))
 
-                    # Create Cursor
-                    cur = mysql.connection.cursor()
-                    exe = cur.execute("UPDATE users SET name=%s, email=%s, password=%s, mobile=%s WHERE id=%s",
-                                      (name, email, password, mobile, q))
-                    if exe:
-                        flash('Profile updated', 'success')
-                        return render_template('user_settings.html', result=result, form=form)
-                    else:
-                        flash('Profile not updated', 'danger')
-                return render_template('user_settings.html', result=result, form=form)
-            else:
-                flash('Unauthorised', 'danger')
-                return redirect(url_for('login'))
-        else:
-            flash('Unauthorised! Please login', 'danger')
-            return redirect(url_for('login'))
-    else:
-        flash('Unauthorised', 'danger')
-        return redirect(url_for('login'))
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
